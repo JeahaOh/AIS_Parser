71f2d25bfc080111d5e7d7dd0a6b0805bc8888c9
JeahaOh <jeaha1217@gmail.com>
2020-05-28 16:18:40 +0900

    mybatis

---
+++ src/main/java/ais/parser/JDBC/ConnectionTest.java
@@ -0,0 +1,22 @@
+package ais.parser.JDBC;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConnectionTest {
+  
+  private static Logger logger = LoggerFactory.getLogger(ConnectionTest.class);
+  
+  private static String getConnectionTest() {
+    SqlSession session = SqlMapClient.getSqlSession();
+    String NOW = session.selectOne("SQL.getServerTime");
+    logger.info("NOW : " + NOW);
+    session.close();
+    return NOW;
+  }
+    
+  public static void main(String[] args) {
+    System.out.println(getConnectionTest());
+  }
+}
---
+++ src/main/java/ais/parser/JDBC/SqlMapClient.java
@@ -0,0 +1,38 @@
+package ais.parser.JDBC;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlMapClient {
+ private static SqlSession session;
+
+// static {
+//    try {
+//      String resource = "myBatisConfig.xml";
+//      Reader reader = Resources.getResourceAsReader(resource);
+//      SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+//      session = sqlMapper.openSession();
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
+//  }
+   
+  public static SqlSession getSqlSession() {
+    if(session == null) {
+      try {
+        String resource = "myBatisConfig.xml";
+        Reader reader = Resources.getResourceAsReader(resource);
+        SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+        session = sqlMapper.openSession();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }      
+    }
+    return session;
+  }
+}
